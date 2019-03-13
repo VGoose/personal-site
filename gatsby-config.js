e3937d69a0a8c3efd0a0277a558fe4a84f213f09
anh <anh@cjdeMacBook-Air.fios-router.home>
2019-03-12 20:33:51 -0400

    edit collections

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,9 +11,10 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/_posts/blog`,
       },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -25,7 +26,6 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
