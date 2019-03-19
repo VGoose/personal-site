2b9b18c41930fda578dcd66fc1867ac3af1be2ad
AnhVoMBP15 <anhvouw@gmail.com>
2019-03-18 21:24:17 -0400

    add to about

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-const SecondPage = () => (
+const About = () => (
   <Layout>
     <SEO title="About Me" />
     <h1>About Me</h1>
@@ -17,8 +17,40 @@ const SecondPage = () => (
       Check out my <a target="_blank" href="https://github.com/vgoose">Github
       </a> forfull details and source code!
     </p>
-    <Link to="/">Go back to the homepage</Link>
+    <Project
+      url='https://github.com/VGoose/react-native-realistic-deck-swiper'
+
+    />
   </Layout>
 )
+const projects = [
+  {
+    name: 'anhvo.io',
+    url: 'https://github.com/VGoose/personal-site',
+    img: '..',
+    desc: `Personal site built with Gatsby and React hooks!`
+  },
+  {
+    name: 'gogonow',
+    url: 'https://github.com/VGoose/gogonow',
+    img: '..',
+    desc: `React Native day planner app with weather and MTA subway times. 
+    Released on iOS Store, release for Google Playstore in development.`
+  },
+  {
+    name: 'React Native Realistic Deck Swiper',
+    url: 'https://github.com/VGoose/react-native-realistic-deck-swiper',
+    img: '..',
+    desc: `Open source animation project built with React Native Animated.
+    Imitates The New Yorker App's cartoons swipe deck.`
+  },
+]
+const Project = ({ name, url, img, desc }) => (
+  <Link to={url}>
+    <img src={img} />
+    <h2>{name}</h2>
+    <p>{desc}</p>
+  </Link>
+)
 
-export default SecondPage
+export default About
